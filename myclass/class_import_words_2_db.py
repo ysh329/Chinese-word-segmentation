22a38d4f58a164c50e3fe264f7ed136cd68e4799
Shuai Yuan <ysh329@sina.com>
2015-07-31 15:02:53 +0800

    Finish the insert task by map method(method #1). Compared with no parallelization method(such as method #2, use the for-loop method), the parallelized method faster 5X than for-loop method.

--- myclass/class_import_words_2_db.py
+++ myclass/class_import_words_2_db.py
@@ -16,22 +16,15 @@ __author__ = 'yuens'
 import MySQLdb
 import sys
 import re
-import time
+from time import clock
 ################################### PART2 CLASS && FUNCTION ###########################
 class import_words_2_db(object):
     def __init__(self):
-        self.start = time.clock()
+        self.start = clock()
 
     def __del__(self):
         self.con.close()
-        self.end = time.clock()
-        print "The function run time is : %.03f seconds" % (self.end - self.start)
-
-    def dbcommit(self):
-        self.con.commit()
-
-    def dbrollback(self):
-        self.con.rollback()
+        print "The function run time is : %.03f seconds" % (clock() - self.start)
 
     def create_database(self, database_name):
         try:
@@ -44,9 +37,9 @@ class import_words_2_db(object):
         sqls = ['SET NAMES UTF8', 'SELECT VERSION()', 'CREATE DATABASE %s' % database_name]
         try:
             map(lambda x: cursor.execute(x), sqls)
-            self.dbcommit()
+            self.con.commit()
         except MySQLdb.Error, e:
-            self.dbrollback()
+            self.con.rollback()
             print 'Fail in creating database %s.' % database_name
             print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
 
@@ -58,7 +51,7 @@ class import_words_2_db(object):
         sqls.append("ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name)
         sqls.append("""CREATE TABLE IF NOT EXISTS %s(
                                 id INT(11) AUTO_INCREMENT PRIMARY KEY,
-                                word VARCHAR(30) NOT NULL,
+                                word TEXT NOT NULL,
                                 pinyin TEXT NOT NULL,
                                 showtimes INT(11) NOT NULL DEFAULT 0,
                                 weight FLOAT(11) NOT NULL DEFAULT 0.0,
@@ -71,24 +64,24 @@ class import_words_2_db(object):
         #print sqls
         try:
             map(lambda x: cursor.execute(x), sqls)
-            self.dbcommit()
+            self.con.commit()
         except MySQLdb.Error, e:
-            self.dbrollback()
+            self.con.rollback()
             print 'Fail in creating %s table.' % table_name
             print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
 
 
     def get_words_from_sogou(self, file_dir, database_name, table_name):
-        f = open(file_dir)
+
         cursor = self.con.cursor()
-        sqls = ["ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name]
-        sqls.append("set character_set_database=utf8")
-        sqls.append("set character_set_filesystem=utf8")
-        sqls.append("set character_set_server=utf8")
+        sqls = ["ALTER DATABASE %s DEFAULT CHARACTER SET 'UTF8'" % database_name]
+        sqls.append("SET CHARACTER_SET_DATABASE=UTF8")
+        sqls.append("SET CHARACTER_SET_FILESYSTEM=UTF8")
+        sqls.append("SET CHARACTER_SET_SERVER=UTF8")
         sqls.append("SET NAMES UTF8")
         try:
             map(lambda sql: cursor.execute(sql), sqls)
-            self.dbcommit()
+            self.con.commit()
         except MySQLdb.Error, e:
             print 'Fail in connecting MySQL.'
             print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
@@ -96,35 +89,63 @@ class import_words_2_db(object):
         #print type(f)
         #print sys.getsizeof(f)
 
+        # method #1 for-loop method of reading files
+        '''
         counter = 0
         success_counter = 0
+        f = open(file_dir)
         for line in f:
             counter += 1
-
-            word = re.compile(' (.*)').findall(line)[0].strip()
+            word = re.compile(' (.*)').findall(line)[0].replace("'", '').strip()
             pinyin = re.compile('(.*) ').findall(line)[0].replace("'", '-').strip()
-            source = "sougou"
+            source = "sogou"
             try:
                 sqls = []
-                #cursor.execute("""INSERT INTO %s(word, pinyin, showtimes, weight, explan, type1, type2, cixing, source)
-                #        VALUES ('%s', '%s', 0, 0.0, '', '', '', '', '%s')""" % (table_name, word, pinyin, source))
                 sqls.append("""INSERT INTO %s(word, pinyin, showtimes, weight, meaning, cixing, type1, type2, source) VALUES('%s', '%s', 0, 0.0, 'ex', 'cx', 't1', 't2', '%s')""" % (table_name, word, pinyin, source))
                 map(lambda sql: cursor.execute(sql), sqls)
-                self.dbcommit()
+                self.con.commit()
                 success_counter += 1
-                if success_counter % 10000 == 0:
+                if success_counter % 1000 == 0:
                     print "#%d successful insert of word #%d." % (success_counter, counter)
             except MySQLdb.Error, e:
-                self.dbrollback()
+                self.con.rollback()
                 print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
 
-        f.close()
         print "summation of words:%d." % counter
         print "success inserted words:%d." % success_counter
         print "insert success rate:%f." % (success_counter / float(counter))
+        '''
 
+        # method #2 map method of reading files
+        f = open(file_dir,"r")
+        lines = f.readlines()
+        word_list = map(lambda line: self.get_word_in_line(line), lines)
+        pinyin_list = map(lambda line: self.get_pinyin_in_line(line), lines)
+        print "len(word_list):", len(word_list)
+        print "len(pinyin_list):", len(pinyin_list)
+        f.close()
+
+        source = "sogou"
+        map(lambda word, pinyin: cursor.execute("""INSERT INTO %s(word, pinyin, showtimes, weight, meaning, cixing, type1, type2, source) VALUES('%s', '%s', 0, 0.0, 'ex', 'cx', 't1', 't2', '%s')""" % (table_name, word, pinyin, source)), word_list, pinyin_list)
+        self.con.commit()
+
+    def get_word_in_line(self, line):
+        word = re.compile(' (.*)').findall(line)[0].replace("'", '').strip()
+        return word
+
+
+    def get_pinyin_in_line(self, line):
+        pinyin = re.compile('(.*) ').findall(line)[0].replace("'", '-').strip()
+        return pinyin
+
+
+################################### PART3 CLASS TEST ##################################
+# initial parameters
+database_name = "wordsDB"
+table_name = "chinese_word_table"
+file_dir = "../data/sogou_cellbase-utf8.txt"
 
 test = import_words_2_db()
-test.create_database(database_name = "chinese_wordsDB")
-test.create_table(database_name= "chinese_wordsDB", table_name = "chinese_word_table")
-test.get_words_from_sogou(file_dir = "../data/sogou_cellbase-utf8.txt", database_name = "chinese_wordsDB", table_name = "chinese_word_table")
+test.create_database(database_name = database_name)
+test.create_table(database_name= database_name, table_name = table_name)
+test.get_words_from_sogou(file_dir = file_dir, database_name = database_name, table_name = table_name)
