4e5183c87569aa343407522a3b43a9a5aea27c37
Shuai Yuan <ysh329@sina.com>
2015-07-31 13:03:14 +0800

    Solve the problem of chinese character display in database.

--- myclass/class_import_words_2_db.py
+++ myclass/class_import_words_2_db.py
@@ -1,10 +1,23 @@
-__author__ = 'yuens'
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+################################### PART0 DESCRIPTION #################################
+# Filename: class_import_words_2_db.py
+# Description:
+#             import file's words of lines in sogou_cellbase-utf8.txt
+#      to database named "chinese_wordsDB"'s table "chinese_words_table",
+#      which contains (id, word, pinyin, explan, )
 
+# Author: Shuai Yuan
+# E-mail: ysh329@sina.com
+# Create: 2015-7-31
+# Last:
+__author__ = 'yuens'
+################################### PART1 IMPORT ######################################
 import MySQLdb
 import sys
 import re
 import time
-
+################################### PART2 CLASS && FUNCTION ###########################
 class import_words_2_db(object):
     def __init__(self):
         self.start = time.clock()
@@ -45,15 +58,16 @@ class import_words_2_db(object):
         sqls.append("ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name)
         sqls.append("""CREATE TABLE IF NOT EXISTS %s(
                                 id INT(11) AUTO_INCREMENT PRIMARY KEY,
-                                word TEXT NOT NULL,
+                                word VARCHAR(30) NOT NULL,
                                 pinyin TEXT NOT NULL,
                                 showtimes INT(11) NOT NULL DEFAULT 0,
                                 weight FLOAT(11) NOT NULL DEFAULT 0.0,
-                                explan TEXT NOT NULL,
+                                meaning TEXT NOT NULL,
+                                cixing VARCHAR(10) NOT NULL,
                                 type1 VARCHAR(30) NOT NULL,
                                 type2 VARCHAR(30) NOT NULL,
                                 source TEXT NOT NULL)""" % table_name)
-        #sqls.append("CREATE INDEX id_idx ON %s(id)" % table_name)
+        sqls.append("CREATE INDEX id_idx ON %s(id)" % table_name)
         #print sqls
         try:
             map(lambda x: cursor.execute(x), sqls)
@@ -62,20 +76,22 @@ class import_words_2_db(object):
             self.dbrollback()
             print 'Fail in creating %s table.' % table_name
             print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
-        finally:
-            self.con.close()
 
 
     def get_words_from_sogou(self, file_dir, database_name, table_name):
         f = open(file_dir)
+        cursor = self.con.cursor()
+        sqls = ["ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name]
+        sqls.append("set character_set_database=utf8")
+        sqls.append("set character_set_filesystem=utf8")
+        sqls.append("set character_set_server=utf8")
+        sqls.append("SET NAMES UTF8")
         try:
-            self.con = MySQLdb.connect(host = "localhost", user = "root", passwd = "931209", db = database_name, charset = "utf8")
-            cursor = self.con.cursor()
-            cursor.execute("ALTER DATABASE %s DEFAULT CHARACTER SET 'utf8'" % database_name)
+            map(lambda sql: cursor.execute(sql), sqls)
+            self.dbcommit()
         except MySQLdb.Error, e:
             print 'Fail in connecting MySQL.'
             print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
-
 
         #print type(f)
         #print sys.getsizeof(f)
@@ -84,14 +100,16 @@ class import_words_2_db(object):
         success_counter = 0
         for line in f:
             counter += 1
-            word = re.compile(' (.*)').findall(line)[0]
-            pinyin = re.compile('(.*) ').findall(line)[0].replace("'", ' ')
+
+            word = re.compile(' (.*)').findall(line)[0].strip()
+            pinyin = re.compile('(.*) ').findall(line)[0].replace("'", '-').strip()
             source = "sougou"
-            # print counter, word, pinyin, source
             try:
-                cursor.execute("SET NAMES UTF8")
-                cursor.execute("""INSERT INTO %s(word, pinyin, showtimes, weight, explan, type1, type2, source)
-                        VALUES ('%s', '%s', 0, 0.0, '', '', '', '%s')""" % (table_name, word, pinyin, source))
+                sqls = []
+                #cursor.execute("""INSERT INTO %s(word, pinyin, showtimes, weight, explan, type1, type2, cixing, source)
+                #        VALUES ('%s', '%s', 0, 0.0, '', '', '', '', '%s')""" % (table_name, word, pinyin, source))
+                sqls.append("""INSERT INTO %s(word, pinyin, showtimes, weight, meaning, cixing, type1, type2, source) VALUES('%s', '%s', 0, 0.0, 'ex', 'cx', 't1', 't2', '%s')""" % (table_name, word, pinyin, source))
+                map(lambda sql: cursor.execute(sql), sqls)
                 self.dbcommit()
                 success_counter += 1
                 if success_counter % 10000 == 0:
@@ -99,6 +117,7 @@ class import_words_2_db(object):
             except MySQLdb.Error, e:
                 self.dbrollback()
                 print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
+
         f.close()
         print "summation of words:%d." % counter
         print "success inserted words:%d." % success_counter
