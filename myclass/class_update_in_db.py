d26c1c8f4f5ffb8532ce63d8c76b47533c307cc0
Shuai Yuan <ysh329@sina.com>
2015-08-04 10:48:13 +0800

    finish the structure design of class_update_in_db.py file. Delay the development of this class.

---
+++ myclass/class_update_in_db.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+################################### PART0 DESCRIPTION #################################
+# Filename: class_update_in_db.py
+# Description:
+#             Update some fields in table of database, such as judge if the word is a
+#       chinese char or english words(based on letter) or a symbol, etc.
+#             This process is like a ETL's pre-process more or less. Besides operation below, it also
+#       contains check and delete the repeated records and abnormal records.
+#             Table structure:(id, word, pinyin, showtimes, weight, meaning, cixing, type1, type2, source)
+
+# Author: Shuai Yuan
+# E-mail: ysh329@sina.com
+# Create: 2015-8-4 08:48:57
+# Last:
+__author__ = 'yuens'
+################################### PART1 IMPORT ######################################
+import MySQLdb
+
+################################### PART2 CLASS && FUNCTION ###########################
+class update_in_db(object):
+    def __init__(self, database_name):
+        try:
+            self.con = MySQLdb.connect(host = "localhost", user = "root", passwd = "931209", db = database_name, charset = "utf8")
+            print "Connect MySQL database successfully."
+        except:
+            print "Connect database failed."
+            print 'MySQL Error %d: %s.' % (e.args[0], e.args[1])
+
+    def __del__(self):
+        self.con.close()
+        print "Quit MySQL database successfully."
+
+    def update_pinyin_field(self):
+        '''recognize_char_type'''
+        pass
+
+    def update_source_field(self):
+        pass
+
+    def update_meaning_field(self):
+        pass
+
+    def update_cixing_field(self):
+        pass
+
+################################### PART3 CLASS TEST ##################################
+database_name = "wordsDB"
+test = update_in_db(database_name = database_name)
